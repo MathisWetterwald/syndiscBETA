6838b44731f30351b59015b7087e53ba7b1bb87b
MathisWetterwald <136447716+MathisWetterwald@users.noreply.github.com>
2023-07-24 22:05:07 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(name="syndisc",
+        packages=find_packages(),
+        provides=["syndisc"],
+        version="0.1",
+        author="Pedro Mediano and Fernando Rosas",
+        url="https://www.information-dynamics.org",
+        install_requires=[
+            "dit>=1.5",
+            "pypoman",
+            "cvxopt",
+            "numpy",
+            "scipy",
+            "networkx>=3.1",
+            "matplotlib",   # Actually required by pypoman
+            ],
+        long_description=open("README.md").read(),
+        license="BSD",
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: BSD License",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Topic :: Scientific/Engineering",
+            "Topic :: Scientific/Engineering :: Mathematics",
+            "Topic :: Scientific/Engineering :: Physics",
+            ],
+        )
