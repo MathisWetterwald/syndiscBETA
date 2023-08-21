3529baa5fa08efc6d775c42e37f819d8ded97fab
MathisWetterwald <136447716+MathisWetterwald@users.noreply.github.com>
2023-08-21 11:28:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name="syndiscBETA",
         url="https://www.information-dynamics.org",
         install_requires=[
             "dit>=1.5",
-            "syndisc @ git+ssh://git@github.com/pmediano/syndisc.git@main",
+            "syndisc @ git+https://github.com/pmediano/syndisc.git@main",
             "pypoman",
             "cvxopt",
             "numpy",
